d7064f97400f725b26ef43b54eacee7da0f816f0
Josafa01 <josafa_correa_fonseca@hotmail.com>
2021-10-19 01:40:39 -0300

    categoria-read foi implementado

--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Categoria } from "../categoria.model";
+import { CategoriaService } from "../categoria.service";
 
 @Component({
-  selector: 'app-categoria-read',
-  templateUrl: './categoria-read.component.html',
-  styleUrls: ['./categoria-read.component.css']
+  selector: "app-categoria-read",
+  templateUrl: "./categoria-read.component.html",
+  styleUrls: ["./categoria-read.component.css"],
 })
 export class CategoriaReadComponent implements OnInit {
 
-  constructor() { }
+  categorias: Categoria[] = [];
+  displayedColumns: string[] = ["id", "nome", "descricao", "acoes"];
+
+  constructor(private service: CategoriaService, private router: Router) {}
 
   ngOnInit(): void {
+    this.findAll();
   }
 
-  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+      console.log(resposta);
+      this.categorias = resposta;
+    });
+  }
+
 }
---
+++ src/app/components/views/categoria/categoria.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Categoria } from './categoria.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  baseUrl: String = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  findAll():Observable<Categoria[]> {
+    const url = `${this.baseUrl}/categorias`
+    return this.http.get<Categoria[]>(url)
+  }
+
+  
+  
+
+}
